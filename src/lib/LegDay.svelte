9f875f840ead0938f7f6c161cfd99328ddad1817
Dave Oliverio <davezer27@gmail.com>
2025-07-28 15:21:34 -0400

    added data table for parlay page with input fom the spread sheet

--- src/lib/LegDay.svelte
+++ src/lib/LegDay.svelte
@@ -1,5 +1,46 @@
+
+<head>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.css"/>
+</head>
 <script>
-  
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        // Ensure jQuery and DataTables are loaded globally
+        const scriptJquery = document.createElement('script');
+        scriptJquery.src = "https://code.jquery.com/jquery-3.7.1.js";
+        document.body.appendChild(scriptJquery);
+
+        const scriptMoment = document.createElement('script');
+        scriptMoment.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js";
+        document.body.appendChild(scriptMoment);
+
+        const scriptDataTables = document.createElement('script');
+        scriptDataTables.src = "https://cdn.datatables.net/2.3.2/js/dataTables.js";
+        document.body.appendChild(scriptDataTables);
+
+        scriptDataTables.onload = () => {
+            // Wait for jQuery and DataTables to be loaded before initializing
+            if (window.$ && window.$.fn.dataTable) {
+                window.$(document).ready(function () {
+                    window.$('#parlayStats').DataTable({
+                        ajax: 'https://script.google.com/macros/s/AKfycby5KreWSmrrF-c9py6W7d5wLNrkvRceOEN7-b2udD4afUJ622vVs55vef-FgshR4CO2/exec',
+                        columns: [
+                            { data: 'GM Name' },
+                            { data: 'GM Team' },
+                            { data: 'Date' },
+                            { data: 'Week' },
+                            { data: 'Group Parlay Bet' },
+                            { data: 'Odds' },
+                            { data: 'Group Parlay Win' },
+                            { data: 'Bet Category 1' },
+                            { data: 'Bet Category 2' }
+                        ]
+                    });
+                });
+            }
+        };
+    });
 </script>
 
 <style>
@@ -17,29 +58,27 @@
         padding: 0 !important;
         box-shadow: 0px 3px 3px -2px var(--boxShadowOne), 0px 3px 4px 0px var(--boxShadowTwo), 0px 1px 8px 0px var(--boxShadowThree);
     }
-
-
-    :global(.linkText) {
-        font-size: 1.1em;
-    }
-
-    /* a {
-        color: var(--g555);
-        font-size: 1.1em;
-        text-decoration: none;
-    } */
-
-    h4 {
-        text-align: center;
-    }
-
     
 </style>
 
 <div class="pageBody">
     <div class="banner">
-        <h4>Leg Day</h4>
+        <h4>Leg Day Parlay</h4>
     </div>
-
-
+    <table id="parlayStats" class="display">
+        <thead>
+            <tr>
+                <th>GM Name</th>
+                <th>GM Team</th>
+                <th>Date</th>
+                <th>Week</th>
+                <th>Group Parlay Bet</th>
+                <th>Odds</th>
+                <th>Group Parlay Result</th>
+                <th>Bet Category 1</th>
+                <th>Bet Category 2</th>
+            </tr>
+        </thead>
+    </table>
 </div>
+
