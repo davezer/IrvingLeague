7103495fbdcd48f15f21e6ab9dcabf4871bd4e19
Dave Oliverio <davezer27@gmail.com>
2025-07-28 15:46:30 -0400

    fixed resources in components.js file

--- src/lib/components.js
+++ src/lib/components.js
@@ -18,6 +18,7 @@ import HomePost from './BlogPosts/HomePost.svelte';
 import FullPost from './BlogPosts/FullPost.svelte';
 import Posts from './BlogPosts/Posts.svelte';
 import Standings from './Standings/index.svelte';
+import Resources from './Resources/index.svelte';
 
 export {
     Nav,
@@ -28,7 +29,7 @@ export {
     Transactions,
     TransactionsPage,
     News,
-    LegDay,
+    Resources,
     MatchupsAndBrackets,
     Pagination,
     Drafts,
