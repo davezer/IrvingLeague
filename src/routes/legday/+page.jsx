6e3134c9b1eb25f5aba0c1aef7f8a1f511cbe34a
Dave Oliverio <davezer27@gmail.com>
2025-07-28 16:50:59 -0400

    new deployment of the data tables

--- src/routes/legday/+page.jsx
+++ src/routes/legday/+page.jsx
@@ -1,7 +1,7 @@
 export function load() {
   return {
     // you could also move these into a separate config file
-    ajaxUrl: 'https://script.google.com/macros/s/AKfycby5KreWSmrrF-c9py6W7d5wLNrkvRceOEN7-b2udD4afUJ622vVs55vef-FgshR4CO2/exec',
+    ajaxUrl: 'https://script.google.com/macros/s/AKfycbwlYoNVAM8u60WsP85yCb2SUb1wTBb-gltwXeyOLs5Ek5PINghBz3IDwQg4RxvnV2W9/exec',
     columns: [
       { data: 'GM Name' },
       { data: 'GM Team' },
